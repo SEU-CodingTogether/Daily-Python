f1c9e8e7bbd94349abc5ecefb5ed86237518aabc
wanghuan <seuwh2018@foxmail.com>
2018-11-02 16:03:14 +0800

    wanghuan first push answer!

--- Level-01-Starting/solutions/s05.py
+++ Level-01-Starting/solutions/s05.py
@@ -1,7 +1,16 @@
 def checkio(data):
-
+    value_int=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    value_roman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    ret="";
+    temp=data;
+    for i in range(len(value_int)):
+        j=0;
+        j=int(temp/value_int[i]);
+        temp=temp-value_int[i]*j;
+        for k in range(j):
+            ret=ret+value_roman[i];
     #replace this for solution
-    return ""
+    return ret
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
